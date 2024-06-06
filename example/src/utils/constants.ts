a89d87c5c268ed0acde6004baba7feb0b0b163c8
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-06-06 23:15:52 +0900

    chore: sync with zondax's shortener

--- example/src/utils/constants.ts
+++ example/src/utils/constants.ts
@@ -1,18 +1,19 @@
 import { SignerPayloadJSON } from "@polkadot/types/types";
 
 export const DEFAULT_PAYLOAD: SignerPayloadJSON = {
-  specVersion: "0x000f4a10",
-  transactionVersion: "0x0000000e",
-  address: "13TtFyPPgw2ZU4TmH8bmR27Q1qTiT6XPTAprUbkgsJEWEjJx",
+  address: "5EXb7e8Kq9m62XTFKVYmGsHFADU4knyFNg6NKJmLKDCz4Gij",
+  //assetId: null,
   blockHash:
-    "0x06668ec48e8fd2f9dd7873e28fd5ad940c1946b5b61adc4ab6d7a0d0d79a7dd9",
-  blockNumber: "0x005e422e",
-  era: "0xe400",
+    "0x67298e0fc6d5a46167e7d56632cdad9217b1a6b605f8caed7562b6f0113bb3e8",
+  blockNumber: "0x00a46448",
+  era: "0x8500",
   genesisHash:
-    "0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f",
+    "0x6408de7737c59c238890533af25896a2c20608d8b380bb01029acb392781063e",
+  //metadataHash: null,
   method:
-    "0x0a0300183982ce80e4b52f2e80aaf36d18b1eba1a32005ffbefd952962227f2f4db30902286bee",
-  nonce: "0x00000001",
+    "0x0403006cf965cfdd16d81eed9bf10c09a9d0da0141ab7a2419d1ca3045002fd11563110700e8764817",
+  //mode: 0,
+  nonce: "0x00000002",
   signedExtensions: [
     "CheckNonZeroSender",
     "CheckSpecVersion",
@@ -21,15 +22,18 @@ export const DEFAULT_PAYLOAD: SignerPayloadJSON = {
     "CheckMortality",
     "CheckNonce",
     "CheckWeight",
-    "ChargeAssetTxPayment",
+    "ChargeTransactionPayment",
+    "CheckMetadataHash",
   ],
+  specVersion: "0x000f7121",
   tip: "0x00000000000000000000000000000000",
+  transactionVersion: "0x0000001a",
   version: 4,
 };
 
 export const DEFAULT_API_URL =
   "https://api.zondax.ch/polkadot/transaction/metadata";
 
-export const DEFAULT_CHAIN_ID = "dot-hub";
+export const DEFAULT_CHAIN_ID = "roc";
 
-export const DEFAULT_WS_URL = "wss://statemint-rpc.dwellir.com";
+export const DEFAULT_WS_URL = "wss://rococo-rpc.polkadot.io";
