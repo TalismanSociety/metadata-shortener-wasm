68525afbe60ebde77d8778495ac5f77b6db319c0
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-06-07 15:36:13 +0900

    chore: add repo url and license details

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+/**
+ * Adapted from https://github.com/Zondax/ledger-polkadot-generic-api/blob/master/rust/src/lib.rs
+ * Changes June 7th 2024 : 
+ * - adjusted get_short_metadata_from_tx_blob signature to be wasm-bindgen compliant
+ * 
+   Copyright 2024 Zondax
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 mod helper;
 
 use parity_scale_codec::{Decode, Encode};
